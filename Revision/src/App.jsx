5509c71967a2e42ea1a6c0c14100272541bcd661
Mohit Yadav <my266620@gmail.com>
2025-11-08 23:15:52 +0530

    .

--- Revision/src/App.jsx
+++ Revision/src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const App = () => {
+  const [num, setnum] = useState(0)
+  function Increment(){
+    setnum(num+1);
+  }
+  function Decrement(){
+    setnum(num-1);
+  }
   return (
     <div>
-      <h1>hello</h1>
+      <h1 className="text-6xl text-blue-600 font-bold">Tailwind Working ✅</h1>
+      <h3>Current value is {num}</h3>
+      <button onMouseDown={Increment}>Increment</button>
+      <button onClick={Decrement}>Decrement</button>
     </div>
   )
 }
--- Revision/src/main.jsx
+++ Revision/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'   // ✅ Tailwind (should be loaded first)
+import './App.css'     // ✅ Component-specific styles (optional)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
