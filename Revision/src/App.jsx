b2d6d6c8d3117329e51442ea4c4ca40d11732008
Mohit Yadav <my266620@gmail.com>
2025-11-23 21:28:07 +0530

    .

--- Revision/src/App.jsx
+++ Revision/src/App.jsx
@@ -1,20 +1,15 @@
 import React,{useState} from 'react'
+import Card from './components/Card'
+
 
 const App = () => {
-  const [num, setnum] = useState(0)
-  function Increment(){
-    setnum(num+1);
-  }
-  function Decrement(){
-    setnum(num-1);
-  }
+ 
   return (
-    <div>
-      <h1 className="text-6xl text-blue-600 font-bold">Tailwind Working ✅</h1>
-      <h3>Current value is {num}</h3>
-      <button onMouseDown={Increment}>Increment</button>
-      <button onClick={Decrement}>Decrement</button>
-    </div>
+    <>
+    <Card a = "Mohit yadav"/>
+  
+  
+    </>
   )
 }
 
---
+++ Revision/src/components/Card2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card2 = (props) => {
+  return (
+    <div>
+      <h1 className='text-white'>Myself {props.user}</h1>
+    </div>
+  )
+}
+
+export default Card2
